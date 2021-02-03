1ecdeb1c13ed01e0f69ee9505079cebc65984c33
micro-hawk <dasvikas0071@gmail.com>
2021-02-03 16:17:35 +0530

    innerHTML in js

--- app.js
+++ app.js
@@ -5,7 +5,25 @@ const todoList = document.querySelector('.todo-list');
 
 
 //Event Listners
-
+todoButton.addEventListener('click' ,addTodo );
 
 
 //Functions
+function addTodo(event){
+    //prevent from submitting ::
+    event.preventDefault();
+    //Todo DIV
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    //create LI
+    const newTodo = document.createElement('li');
+    newTodo.innerText = 'heyt';
+    newTodo.classList.add('todo-items');
+
+    todoDiv.appendChild(newTodo);
+
+    //CheckMark button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = "<i>fas fa-check </i>";
+    completedButton.classList.add('complete-btn');
+}
